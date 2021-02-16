f0ca4dc5c642bbd6ff42df7cff9ce2eaea344315
A01706202 <A01706202@itesm.mx>
2021-02-16 09:30:14 -0600

    Avance Lab 3

--- Lab_5/Lab_5.js
+++ Lab_5/Lab_5.js
@@ -1,8 +1,9 @@
+
 function validar_password() {
     let boton = document.getElementById("Validacion");
     boton.onclick = () => {
-        let password = document.getElementById("password");
-        let val_password = document.getElementById("validar_password");
+        let password = document.getElementById("password").value;
+        let val_password = document.getElementById("validar_password").value;
         if (password == val_password){
             window.alert("Stonks");
         } else {
