a7a9274bc8f325499e67784664954b0ce5110cc7
A01706202 <A01706202@itesm.mx>
2021-02-16 23:03:17 -0600

    Validación de contraseñas

--- Lab_5/Lab_5.js
+++ Lab_5/Lab_5.js
@@ -1,14 +1,26 @@
 
+let password = document.getElementById("password");
+password.onkeyup = () => {  
+    valor_password = password.value;
+}
+
+let password2 = document.getElementById("password2");
+password2.onkeyup = () => {
+    valor_password2 = password2.value;
+}
+  
+
 function validar_password() {
     let boton = document.getElementById("Validacion");
-    boton.onclick = () => {
-        let password = document.getElementById("password").value;
-        let val_password = document.getElementById("validar_password").value;
-        if (password == val_password){
+    /*if ((valor_password || valor_password2) == null){
+        alert("Llene los campos por favor");
+    } else {*/
+        if (valor_password == valor_password2){
+            console.log("yes")
             window.alert("Stonks");
         } else {
             window.alert("Las contraseñas no coinciden");
         }
-    }
+   // }
+    
 }
-
