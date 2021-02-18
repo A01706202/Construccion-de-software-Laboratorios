249ae94c9dd60050cd3083cd934d1d4d812d454e
A01706202 <A01706202@itesm.mx>
2021-02-18 01:24:24 -0600

    password

--- Lab_5/Lab_5.js
+++ Lab_5/Lab_5.js
@@ -1,3 +1,4 @@
+document.getElementById("preguntas").classList.add("ocultar");
 
 let password = document.getElementById("password");
 password.onkeyup = () => {  
@@ -8,15 +9,19 @@ let password2 = document.getElementById("password2");
 password2.onkeyup = () => {
     valor_password2 = password2.value;
 }
-  
 
 function validar_password() {
     let boton = document.getElementById("Validacion");
+    
 
         if (valor_password == valor_password2){
-            window.alert("Stonks");
+            window.alert("¡Contraseña validada!");
             document.getElementById("pass").classList.add("ocultar");
-            document.getElementById("tienda");
+            document.getElementById("preguntas").classList.remove("ocultar");
+            let album1 = document.getElementById("tienda");
+            album1.innerHTML = '<img alt="Yung Bae" src="https://www.google.com.mx/url?sa=i&url=https%3A%2F%2Fwww.discogs.com%2FYung-Bae-Bae%2Frelease%2F13848176&psig=AOvVaw1fhwtWMzbew3U5rcaCNGZR&ust=1613631332595000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCKj0lqar8O4CFQAAAAAdAAAAABAO">'
+            
+            
         } else {
             window.alert("Las contraseñas no coinciden");
         }
