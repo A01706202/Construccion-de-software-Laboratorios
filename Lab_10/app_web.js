3d7d9ed0c4cb88b02bde6afcbe58b2b613ac9500
A01706202 <A01706202@itesm.mx>
2021-02-22 21:39:57 -0600

    Ruta principal y ruta 1

--- Lab_10/app_web.js
+++ Lab_10/app_web.js
@@ -3,26 +3,36 @@ const http = require('http');
 const server = http.createServer((request, response)=>{
 
     if (request.url === "/"){
-        response.setHeader("Content-Type','text/html");
+        response.setHeader('Content-Type','text/html');
         response.write("<html>");
         response.write("<head><title>App Web</title></head>");
         response.write("<body><h1><ul>Laboratorio 10</ul></h1>");
-        response.write("Michelle Aulin Calzada Montes<br> A01706202<br> ITC<br><br>");
+        response.write("Michelle Aylin Calzada Montes<br> A01706202<br> ITC<br><br>");
+        response.write("<h2>Rutas disponibles</h2>");
+        response.write("<li>/ruta1</li>");
+        response.write("<li>/ruta2</li>");
+        response.write("<li>/ruta3</li>");
         response.write("</body>");
         response.write("</html>");
         response.end();
         
     } else if (request.url === "/ruta1"){
-        response.setHeader("Content-Type','text/html");
+        response.setHeader('Content-Type','text/html');
         response.write("<html>");
         response.write("<head><title>App Web</title></head>");
-        response.write("<body>");
+        response.write("<body><h1><ul>Ruta 1</ul></h1>");
+        response.write("Hola!!! Llegaste a la ruta 1 :)<br><br>");
+        response.write("Sabias que en realidad si existe una clase de murcielagos que se alimentan de sangre?<br>");
+        response.write("Esta subfamilia se le llama Murcielagos vampiros y hay tres especies:");
+        response.write("<li>Vampiro comun</li>");
+        response.write("<li>Vampiro patas peludas</li>");
+        response.write("<li>Vampiro alas blancas</li>");
         response.write("</body>");
         response.write("</html>");
         response.end();
 
     } else if (request.url === "/ruta2") {
-        response.setHeader("Content-Type','text/html");
+        response.setHeader('Content-Type','text/html');
         response.write("<html>");
         response.write("<head><title>App Web</title></head>");
         response.write("<body>");
@@ -31,7 +41,7 @@ const server = http.createServer((request, response)=>{
         response.end();
 
     } else if (request.url === "/ruta3") {
-        response.setHeader("Content-Type','text/html");
+        response.setHeader('Content-Type','text/html');
         response.write("<html>");
         response.write("<head><title>App Web</title></head>");
         response.write("<body>");
@@ -40,10 +50,11 @@ const server = http.createServer((request, response)=>{
         response.end();
 
     } else {
-        response.setHeader("Content-Type','text/html");
+        response.statusCode = 404;
+        response.setHeader('Content-Type','text/html');
         response.write("<html>");
-        response.write("<head><title>App Web</title></head>");
-        response.write("<body>");
+        response.write("<head><title>Page not found</title></head>");
+        response.write("<body><h1>Page not found :(</h1>");
         response.write("</body>");
         response.write("</html>");
         response.end();
