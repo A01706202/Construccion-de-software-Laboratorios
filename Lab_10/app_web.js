69690bcfe4489f8b63097d168912d2f7ada1ee89
A01706202 <A01706202@itesm.mx>
2021-02-23 22:49:45 -0600

    Laboratorio terminado

--- Lab_10/app_web.js
+++ Lab_10/app_web.js
@@ -9,18 +9,18 @@ const server = http.createServer((request, response)=>{
         response.write("<body><h1><ul>Laboratorio 10</ul></h1>");
         response.write("Michelle Aylin Calzada Montes<br> A01706202<br> ITC<br><br>");
         response.write("<h2>Rutas disponibles</h2>");
-        response.write("<li>/ruta1</li>");
-        response.write("<li>/ruta2</li>");
-        response.write("<li>/ruta3</li>");
+        response.write("<li>/murcielagos-vampiro</li>");
+        response.write("<li>/dato-curioso</li>");
+        response.write("<li>/mandar-sugerencia</li>");
         response.write("</body>");
         response.write("</html>");
         response.end();
         
-    } else if (request.url === "/ruta1"){
+    } else if (request.url === "/murcielagos-vampiro"){
         response.setHeader('Content-Type','text/html');
         response.write("<html>");
         response.write('<head><meta charset="UTF-8"><title>App Web</title></head>');
-        response.write("<body><h1><ul>Ruta 1</ul></h1>");
+        response.write("<body><h1><ul>Murcielagos vampiro</ul></h1>");
         response.write("Hola!!! Llegaste a la ruta 1 :)<br><br>");
         response.write("¿Sabías que en realidad si existe una clase de murciélagos que se alimentan de sangre?<br>");
         response.write("Esta subfamilia se le llama Murciélagos vampiros y hay tres especies:");
@@ -31,30 +31,31 @@ const server = http.createServer((request, response)=>{
         response.write("</html>");
         response.end();
 
-    } else if (request.url === "/ruta2") {
+    } else if (request.url === "/dato-curioso") {
         response.setHeader('Content-Type','text/html');
         response.write("<html>");
         response.write('<head><meta charset="UTF-8"><title>App Web</title></head>');
-        response.write("<body><h1><ul>Ruta 2</ul></h1>");
+        response.write("<body><h1><ul>Dato curioso</ul></h1>");
         response.write("Que bien! Llegaste a la ruta 2 :o<br><br>");
-        response.write("¿Sabías que puedes beber lava pero solo una vez en la vida?");
+        response.write("¿Sabías que puedes beber lava pero solo una vez en la vida?<br><br>");
+        response.write('<img alt="Lava" src="https://upload.wikimedia.org/wikipedia/commons/8/82/Pahoehoe_toe.jpg">');
         response.write("</body>");
         response.write("</html>");
         response.end();
 
-    } else if (request.url === "/ruta3" && request.method === "GET") {
+    } else if (request.url === "/mandar-sugerencia" && request.method === "GET") {
         response.setHeader('Content-Type','text/html');
         response.write("<html>");
         response.write('<head><meta charset="UTF-8"><title>App Web</title></head>');
         response.write("<body><h1><ul>Ruta 3</ul></h1>");
-        response.write("¡Bienvenido a la ruta 3!<br><br>");
+        response.write("¡Bienvenido a la caja de sugerencias!<br><br>");
         response.write("Envía tus sugerencias y quejas de esta app web, nos importa su opinión c:<br><br>");
-        response.write('<form action="ruta3" method="POST"><input type="text" name="Opinion"><input type="submit" value="Enviar"></form>')
+        response.write('<form action="mandar-sugerencia" method="POST"><input type="text" name="Opinion"><input type="submit" value="Enviar"></form>')
         response.write("</body>");
         response.write("</html>");
         response.end();
     
-    } else if (request.url === "/ruta3" && request.method === "POST"){
+    } else if (request.url === "/mandar-sugerencia" && request.method === "POST"){
         const sugerencias = [];
         const fs = require('fs');
         request.on('data', (dato) => {
@@ -63,15 +64,12 @@ const server = http.createServer((request, response)=>{
         return request.on('end', () => {
             const sugerencias_completas = Buffer.concat(sugerencias).toString();
             const opinion = sugerencias_completas.split("=")[1];
-            const opinion_completa = opinion.split("+");
-            for (let i=0; i<opinion_completa.length; i++){
-                fs.appendFile('texto.txt', opinion_completa[i]+" ", (error)=>{
-                    if(error){
-                        throw error;
-                    }
-                });
-            }
-            return response.setHeader('Location', '/');
+            fs.appendFile('texto.txt', opinion+"\n", (error)=>{
+                if(error){
+                    throw error;
+                }
+            });
+            response.end();
         });
 
     } else {
