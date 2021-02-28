049edc79e99768540b2eaa2404d48aea186a4217
A01706202 <A01706202@itesm.mx>
2021-02-27 22:17:04 -0600

    Carga de archivo texto

--- Lab_11/app.js
+++ Lab_11/app.js
@@ -14,11 +14,11 @@ app.use('/ruta1', (request, response, next) => {
     response.send('Respuesta de la ruta "/ruta"'); 
 });
 
-app.get('/mandar-sugerencias', (request, response, next) => {
+app.get('/mandar-sugerencia', (request, response, next) => {
     response.send('<h1>¡Caja de sugerencias!</h1> <br>Envía tus sugerencias y quejas de esta app web, nos importa su opinión c:<br><br> <form action="mandar-sugerencia" method="POST"><input type="text" name="Opinion"><input type="submit" value="Enviar"></form>');
 });
 
-app.use('/mandar-sugerencias', (request, response, next) => {
+app.post('/mandar-sugerencia', (request, response, next) => {
     console.log(request.body.Opinion);
     const fs = require('fs');
     fs.appendFile('texto.txt', request.body.Opinion+"\n", (error)=>{
@@ -29,13 +29,12 @@ app.use('/mandar-sugerencias', (request, response, next) => {
     response.send('¡Sugerencia mandada con éxito!');
 });
 
-app.use('/', (request, response, next) => {
-    response.send('<h1>Laboratorio 11</h1> <br>Rutas discponibles:<br><br><li>/mandar-sugerencias</li>'); 
+app.get('/', (request, response, next) => {
+    response.send('<h1>Laboratorio 11</h1> Michelle Aylin Calzada Montes<br>A01706202<br>ITC<br> <br><br>Rutas disponibles:<br><br><li>/mandar-sugerencias</li>'); 
 });
 
 app.use((request, response, next) => {
-    console.log('Otro middleware!');
-    response.send('¡Hola mundo!'); //Manda la respuesta
+    response.send('<h1>Page not found :(</h1>');
 });
 
 app.listen(3000);
